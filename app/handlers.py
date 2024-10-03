6c8c284a1649b66ed2bd1e3a0949f2aca7b6eca2
Alexander <alexandervolzhanin2004@gmail.com>
2024-10-03 11:59:32 +0300

    1.7a2: Добавлен Redis

--- app/handlers.py
+++ app/handlers.py
@@ -152,7 +152,7 @@ async def process_generation(message: Message, state: FSMContext, bot: Bot):
                 parse_mode="Markdown"
             )     
             await message.answer(
-                f"Model: {model}\nNumber of tokens per input: {count_tokens(user_input)}\nNumber of tokens per output: {count_tokens(part)}"
+                f"Model: {model}\nNumber of tokens per input: {count_tokens(user_input)}\nNumber of tokens per output: {count_tokens(part)}\nVersion: 1.7a"
                 )
         logger.info("Ответ gpt получен и отправлен пользователю")
         await state.set_state(Generate.text_input)
