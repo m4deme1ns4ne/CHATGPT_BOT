6577331a5a30411111089d08be90578bfd190805
Alexander <alexandervolzhanin2004@gmail.com>
2024-10-15 13:47:13 +0300

    3.2a: добавил символ в экранирование

--- app/escape_markdown.py
+++ app/escape_markdown.py
@@ -5,5 +5,5 @@ def escape_markdown(text: str) -> str:
     """
     Экранировать символы в Markdown-разметке для передачи в Telegram.
     """
-    escape_chars = r'_[]()~>#+-=|{}.!'
+    escape_chars = r'_[]()~`>#+-=|{}.!'
     return re.sub(f'([{re.escape(escape_chars)}])', r'\\\1', text)
