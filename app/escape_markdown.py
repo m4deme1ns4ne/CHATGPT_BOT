8abd3b2466bbc43b76b62dd886007b83412db6a0
Alexander <alexandervolzhanin2004@gmail.com>
2024-10-15 14:56:20 +0300

    3.3a: Убрал маркдаун

--- app/escape_markdown.py
+++ app/escape_markdown.py
@@ -3,7 +3,7 @@ def escape_markdown(text: str) -> str:
     import re
     
     """
-    Экранировать символы в Markdown-разметке для передачи в Telegram.
+    Экранировать символы в Markdown V2 для передачи в Telegram.
     """
-    escape_chars = r'_[]()~`>#+-=|{}.!'
+    escape_chars = r'_.*[]()~`>#+-=|{}.!'
     return re.sub(f'([{re.escape(escape_chars)}])', r'\\\1', text)
